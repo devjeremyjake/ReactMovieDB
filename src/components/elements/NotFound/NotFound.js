db6d54ae9cfde20995e89f2348639b987af2ddaf
devjeremyjake <devjeremyjake@gmail.com>
2019-06-27 12:24:41 +0100

    first commit

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <div>
+            <h1>WHOOOOOPS. This page dosen't exist</h1>
+        </div>
+    )
+}
+
+export default NotFound;
